43ee82ff3921029544e4bcdb08d30d15d64b9d2e
ismaeltec123 <ismael.quispe@tecsup.edu.pe>
2024-09-05 15:08:59 -0500

    Datos BD

--- djangoApp03/lab03/encuesta/models.py
+++ djangoApp03/lab03/encuesta/models.py
@@ -1,3 +1,11 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+# Fecha : 2024-2
+#Encargados : Juan León - Jaime Farfan
+class Pregunta (models.Model):
+    pregunta_texto=models.CharField(max_length=100)
+    pub_date=models.DateField('fecha de publicacion')
+class Opcion (models.Model):
+    pregunta=models.ForeignKey(Pregunta, on_delete=models.CASCADE) 
+    opcion_texto=models.CharField(max_length=100) 
+    votos=models.IntegerField(default=0)
