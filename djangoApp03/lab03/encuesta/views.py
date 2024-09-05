db2b5b83c13bff6e2a0965d3bd9cf85b9057dca5
ismaeltec123 <ismael.quispe@tecsup.edu.pe>
2024-09-05 15:37:23 -0500

    v.0.1

--- djangoApp03/lab03/encuesta/views.py
+++ djangoApp03/lab03/encuesta/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .models import Opcion,Pregunta
 
-# Create your views here.
+
+def index(request):
+    Lista_de_preguntas=Pregunta.objects.order_by('-pub_date')
+    context ={
+        'question_list':Lista_de_preguntas
+    }
+    return render(request,'encuesta/index.html',context)
+
+def detalle(request,pregunta_id):
+    pregunta=Pregunta.objects.get(pk=pregunta_id)
+    context={
+        'pregunta':pregunta
+    }
+    return render(request,'encuesta/detalle.html',context)
+
+def votar(request,pregunta_id):
+    pregunta=Pregunta.objects.get(pk=pregunta_id)
+    opcionSeleccionada=pregunta.opcion_set.get(pk=request.POST['opcion'])
+    opcionSeleccionada.votos+=1
+    opcionSeleccionada.save()
+    context={
+        'pregunta':pregunta
+    }
+    return render(request,'encuesta/resultado.html',context)
